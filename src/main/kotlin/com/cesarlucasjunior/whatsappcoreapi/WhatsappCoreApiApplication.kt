a3ee82d5212b98b424c9f15c50a963f684e23000
César Lucas Júnior <cesarlucasjunior93@gmail.com>
2023-10-19 15:20:30 -0300

    Project setup

---
+++ src/main/kotlin/com/cesarlucasjunior/whatsappcoreapi/WhatsappCoreApiApplication.kt
@@ -0,0 +1,11 @@
+package com.cesarlucasjunior.whatsappcoreapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatsappCoreApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatsappCoreApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/cesarlucasjunior/whatsappcoreapi/WhatsappCoreApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cesarlucasjunior.whatsappcoreapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhatsappCoreApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
